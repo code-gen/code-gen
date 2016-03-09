69df0b6bd3292b1658108f6d59e4abec4ed486a1
Pradeep Kumar Tippa <pktippa@gmail.com>
2016-03-09 18:54:43 +0530

    Initial approach for cli, serve static content

---
+++ bin/cg.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/**
+* Module dependencies.
+*/
+
+var cgCommander = require('commander');
+var express = require('express');
+var app = express();
+
+// CLI Commands
+cgCommander
+  .command('server')
+  .description('Starts the server and give the URL for Web Interface.')
+  .action(function() {
+    // Start the Web server to serve static file content
+	console.log('Server started please browse the Web Interface on http://localhost:3030/cgserver');
+    console.log('Press CTRL + C to stop the server.');
+    // Using express for serving static content and running web server
+    app.use('/cgserver',express.static(__dirname + '/../client'));
+    app.listen(process.env.PORT || 3030);
+  });
+
+//Displaying default help when no options/commands provided. 
+if(!process.argv.slice(2).length) {
+  cgCommander.outputHelp(function(txt){return txt;});
+}
+
+// Passing command line arguments to commander to parse
+cgCommander.parse(process.argv);
